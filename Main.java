42184c75144f70c7e2d4cbfd42de74ec8f60b411
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-02 23:19:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Test numberOfLeapYears
         System.out.println("Number of leap years between 2000 and 2025 (inclusive): " +
                            APCalendar.numberOfLeapYears(2000, 2025));
 
-        // Test dayOfWeek
+    
         System.out.println("Day of the week for January 5, 2019: " +
                            APCalendar.dayOfWeek(1, 5, 2019));
         System.out.println("Day of the week for January 10, 2019: " +
                            APCalendar.dayOfWeek(1, 10, 2019));
 
-        // Additional tests for dayOfWeek (using the current date for verification)
         System.out.println("Day of the week for May 2, 2025: " +
                            APCalendar.dayOfWeek(5, 2, 2025)); // Should be Friday (4)
     }
