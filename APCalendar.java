16e12d34bd684e6e1c2e66915712274c2a1e60a1
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-02 23:20:38 -0400

    Update APCalendar.java

--- APCalendar.java
+++ APCalendar.java
@@ -1,6 +1,6 @@
 public class APCalendar
 {
-    /** Returns true if year is a leap year and false otherwise. */
+
     private static boolean isLeapYear(int year)
     {
         boolean four = (year % 4 == 0);
@@ -9,10 +9,6 @@ public class APCalendar
         return four && (!hundred || fourHundred);
     }
 
-    /** Returns the number of leap years
-     * between year1 and year2, inclusive.
-     * Precondition: 0 <= year1 <= year2
-     */
     public static int numberOfLeapYears(int year1, int year2)
     {
         int count = 0;
@@ -24,31 +20,22 @@ public class APCalendar
         return count;
     }
 
-    /** Returns the value representing the day
-     * of the week for the first day of year,
-     * where 0 denotes Sunday, 1 denotes Monday,
-     * ..., and 6 denotes Saturday.
-     */
+
     private static int firstDayOfYear(int year)
     {
-        int day = 1; // Assume January 1, 0001 was a Monday.
+        int day = 1; 
         for (int i = 1; i < year; i++)
         {
             day++;
             if (isLeapYear(i))
             {
-                day++; // Add an extra day for leap years
+                day++;
             }
         }
         return day % 7;
     }
 
-    /** Returns n, where month, day, and year specify
-     * the nth day of the year. Returns 1 for January 1
-     * (month = 1, day = 1) of any year.
-     * Precondition: The date represented by month, day,
-     * year is a valid date.
-     */
+    
     private static int dayOfYear(int month, int day, int year)
     {
         int days = 0;
@@ -68,18 +55,10 @@ public class APCalendar
         return days;
     }
 
-    /** Returns the value representing the day of the week for
-     * the given date (month, day, year), where 0 denotes Sunday,
-     * 1 denotes Monday, ..., and 6 denotes Saturday.
-     * Precondition: The date represented by month, day,
-     * year is a valid date.
-     */
     public static int dayOfWeek(int month, int day, int year)
     {
         int days = dayOfYear(month, day, year);
         return (firstDayOfYear(year) + days - 1) % 7;
     }
 
-    // There may be instance variables, constructors,
-    // and other methods not shown.
 }
